166083dfb5948132bd1c124a89f6c2e52ac34dc0
marcio <marcio.albu@alu.ufc.br>
2020-07-12 20:11:39 -0300

    *Changes: - Creation of Activity Favorite

--- app/src/main/java/com/example/beerlist/MainActivity.java
+++ app/src/main/java/com/example/beerlist/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.beerlist;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -23,6 +27,32 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+
+            case R.id.action_favorite:
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite...
+                Intent intent = new Intent(MainActivity.this,Favorite.class);
+                startActivity(intent);
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 
     //Function to detect whether there is an Internet
